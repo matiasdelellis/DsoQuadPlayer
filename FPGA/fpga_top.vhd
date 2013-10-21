ed8d03c8db5751d134fe8780599b23047928891b
matiasdelellis <mati86dl@gmail.com>
2013-10-21 17:36:52 -0300

    Add fsmc example..
    Works more or less correctly... haha..
    Rename test_fpga.pawn to pio_example.pawn.

--- FPGA/fpga_top.vhd
+++ FPGA/fpga_top.vhd
@@ -42,24 +42,45 @@ ENTITY dso_quad_top IS
 END dso_quad_top;
 
 ARCHITECTURE Behavior OF dso_quad_top IS
-	SIGNAL counter : UNSIGNED         (26 DOWNTO 0); -- 2^26 < 72000000 < 2^27
-	SIGNAL temp    : STD_LOGIC_VECTOR (8 DOWNTO 0);
-	SIGNAL cntsec  : UNSIGNED         (8 DOWNTO 0);
+	-- 9 bit counter example on PIO
+	SIGNAL counter          : UNSIGNED         (26 DOWNTO 0); -- 2^26 < 72000000 < 2^27
+	SIGNAL temp             : STD_LOGIC_VECTOR (8 DOWNTO 0);
+	SIGNAL cntsec           : UNSIGNED         (8 DOWNTO 0);
+
+	-- Basic fsmc example
+	SIGNAL fsmc_want_count  : STD_LOGIC;
+
+	SIGNAL fifo_data_out    : STD_LOGIC_VECTOR(15 downto 0);
+	SIGNAL address          : UNSIGNED (3 DOWNTO 0);
+	SIGNAL fifo_count       : UNSIGNED (3 DOWNTO 0);
+
+	SIGNAL fsmc_output_data : STD_LOGIC_VECTOR(15 downto 0);
+
 	BEGIN
-		PROCESS (clk)
-		BEGIN
-			IF (rising_edge(clk)) THEN
-				IF (rst_n = '0') THEN
+		-------------------------------
+		-- Fix floating pin warnings --
+		-------------------------------
+		adc_sleep <= '1';
+		cha_clk   <= '0';
+		chb_clk   <= '0';
+
+		-----------------
+		-- PIO example --
+		-----------------
+ 		PROCESS (clk)
+ 		BEGIN
+ 			IF (rising_edge(clk)) THEN
+ 				IF (rst_n = '0') THEN
 					cntsec  <= (OTHERS => '0');
 					counter <= (OTHERS => '0');
-				ELSE
+ 				ELSE
 					counter <= counter + 1;
 					IF (counter = 72000000) THEN
 						cntsec <= cntsec + 1;
 						counter <= (OTHERS => '0');
-					END IF;
-				END IF;
-			END IF;
+ 					END IF;
+ 				END IF;
+ 			END IF;
 		END PROCESS;		
 
 		temp <= STD_LOGIC_VECTOR (cntsec);
@@ -72,4 +93,65 @@ ARCHITECTURE Behavior OF dso_quad_top IS
 		PA7 <= temp (2);
 		PC4 <= temp (1);
 		PC5 <= temp (0);
+
+		-------------------
+		-- FSMC example. --
+		-------------------
+
+		fifo_count <= to_unsigned(14, fifo_count'length);  -- Fixed as "MATIASDELELLIS" lenght.
+
+		-- Process to read fsmc from micro.
+		-- Read on rising_edge(clk), when fsmc_ce = '1' AND fsmc_nwr = '0'
+		-- Used only to detect when micro want the fifo width.
+		PROCESS (clk, rst_n)
+		BEGIN
+			IF rst_n = '0' THEN
+				fsmc_want_count <= '0';
+			ELSIF rising_edge(clk) THEN
+				IF fsmc_ce = '1' AND fsmc_nwr = '0' THEN
+					fsmc_want_count <= fsmc_db (0);
+				END IF;
+			END IF;
+		END PROCESS;
+
+		-- Process to configure the address to read on fifo.
+		-- Change the pointer when rising_edge(clk).
+		-- TODO: Change on any rising_edge(). When have to progress?.
+		PROCESS (clk)
+		BEGIN
+			IF (rising_edge(clk)) THEN
+				IF (rst_n = '0') THEN
+					address <= (OTHERS => '0');
+				ELSE
+					IF (address < 13) THEN
+						address <= address + 1;
+					ELSE
+						address <= (OTHERS => '0');
+					END IF;
+				END IF;
+			END IF;
+		END PROCESS;
+
+		-- Select the info that would go to the port according to address.
+		WITH address SELECT
+			fifo_data_out <= X"0077" WHEN "0000", -- M
+			                 X"0065" WHEN "0001", -- A
+			                 X"0084" WHEN "0010", -- T
+			                 X"0073" WHEN "0011", -- I
+			                 X"0065" WHEN "0100", -- A
+			                 X"0083" WHEN "0101", -- S
+			                 X"0068" WHEN "0110", -- D
+			                 X"0069" WHEN "0111", -- E
+			                 X"0076" WHEN "1000", -- L
+			                 X"0069" WHEN "1001", -- E
+			                 X"0076" WHEN "1010", -- L
+			                 X"0076" WHEN "1011", -- L
+			                 X"0073" WHEN "1100", -- I
+			                 X"0083" WHEN OTHERS; -- S
+
+		-- Select between the information on the fifo, and their size.
+		fsmc_output_data <= fifo_data_out WHEN (fsmc_want_count = '0') ELSE "000000000000" & STD_LOGIC_VECTOR (fifo_count);
+
+		-- Write data on fsmc when fsmc_nrd = '0' and fsmc_ce = '1'. If not, put high impedance to read.
+		fsmc_db <= fsmc_output_data WHEN (fsmc_nrd = '0' AND fsmc_ce = '1') ELSE (OTHERS => 'Z');
 END Behavior;
