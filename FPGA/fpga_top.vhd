215b1522a974482824f03f513bf0bb4a2c6c8d34
matiasdelellis <mati86dl@gmail.com>
2013-10-29 11:53:05 -0300

    Clean fsmc signals and and memory handling.
    RAM: Write on rising_edge and read on falling_edge!.

--- FPGA/fpga_top.vhd
+++ FPGA/fpga_top.vhd
@@ -45,20 +45,16 @@ END dso_quad_top;
 ARCHITECTURE Behavior OF dso_quad_top IS
 	-- FSMC Bus
 	SIGNAL fsmc_want_count  : STD_LOGIC;
-	SIGNAL fsmc_was_read_r  : STD_LOGIC;
 
 	SIGNAL fsmc_output_data : STD_LOGIC_VECTOR(15 downto 0);
 	SIGNAL fsmc_input_data  : STD_LOGIC_VECTOR(15 downto 0);
-	SIGNAL fsmc_nrd_edge    : STD_LOGIC;
 
 	-- Memory
 	TYPE mem_type IS ARRAY (ram_lenght - 1 DOWNTO 0) OF STD_LOGIC_VECTOR (15 downto 0);
 
 	SIGNAL RAM              : mem_type;
-	SIGNAL mem_read         : STD_LOGIC;
 	SIGNAL w_address        : INTEGER RANGE 0 TO ram_lenght - 1;
 	SIGNAL r_address        : INTEGER RANGE 0 TO ram_lenght - 1;
-	SIGNAL l_address        : INTEGER RANGE 0 TO ram_lenght - 1;
 	SIGNAL w_data           : STD_LOGIC_VECTOR(15 downto 0);
 	SIGNAL r_data           : STD_LOGIC_VECTOR(15 downto 0);
 	SIGNAL d_count          : STD_LOGIC_VECTOR(15 downto 0);
@@ -77,54 +73,31 @@ ARCHITECTURE Behavior OF dso_quad_top IS
 
  		PROCESS (clk)
  		BEGIN
- 			IF rising_edge(clk) THEN
- 				IF rst_n = '0' THEN
- 					w_address <= 0;
- 				ELSE
- 					IF PB0 = '1' THEN
- 						IF w_address < ram_lenght - 1 THEN
-	 						w_address <= w_address + 1;
-	 						RAM(w_address) <= w_data;
-	 					END IF;
-					END IF;
- 				END IF;
- 			END IF;
-		END PROCESS;
-
-		-- Infer a lach to save last write address.
- 		PROCESS (clk)
- 		BEGIN
- 			IF rising_edge(clk) THEN
- 				IF (rst_n = '0') THEN
-					l_address <= 0;
- 				ELSE
- 					IF PB0 = '1' THEN
-						l_address <= w_address;
-					END IF;
- 				END IF;
- 			END IF;
-		END PROCESS;
-		d_count <= STD_LOGIC_VECTOR (to_unsigned(l_address, 16));
-
-		-- Read process.
-		PROCESS (clk)
-		BEGIN
 			IF rising_edge(clk) THEN
 				IF rst_n = '0' THEN
-					r_address <= 0;
-				ELSIF mem_read = '1' THEN
-					IF r_address < l_address THEN
-						r_address <= r_address + 1;
+					w_address <= 0;
+ 				ELSIF PB0 = '1' THEN
+ 					IF w_address < ram_lenght - 1 THEN
+	 					w_address <= w_address + 1;
+	 					RAM(w_address) <= w_data;
 					END IF;
+ 				END IF;
+ 			END IF;
+		END PROCESS;
+		d_count <= STD_LOGIC_VECTOR (to_unsigned(w_address, 16));
+
+		-- Read process.
+		PROCESS (fsmc_nrd)
+		BEGIN
+			IF falling_edge(fsmc_nrd) THEN
+				IF rst_n = '0' OR fsmc_want_count = '1' THEN
+					r_address <= 0;
+				ELSIF r_address < w_address THEN
+					r_address <= r_address + 1;
 				END IF;
 			END IF;
 		END PROCESS;
 		r_data <= RAM(r_address);
-		mem_read <= fsmc_nrd_edge WHEN (fsmc_want_count = '0') ELSE '0';
-
-		-------------------
-		-- FSMC example. --
-		-------------------
 
 		-- Micro write Bus on clock when fsmc_ce = '1' AND fsmc_nwr = '0'
 		-- Only used to know when micro want count or mem data.
@@ -140,25 +113,9 @@ ARCHITECTURE Behavior OF dso_quad_top IS
 		END PROCESS;
 		fsmc_want_count <= '1' WHEN fsmc_input_data = X"0001" ELSE '0';
 
-		-- Micro read Bus on clock when fsmc_ce = '1' AND fsmc_nwr = '0'
-		PROCESS (clk, rst_n)
-		BEGIN
-			IF rst_n = '0' THEN
-				fsmc_was_read_r <= '0';
-			ELSIF rising_edge(clk) THEN
-				IF fsmc_ce = '1' AND fsmc_nrd = '0' THEN
-					fsmc_was_read_r <= '1';
-				ELSE
-					fsmc_was_read_r <= '0';
-				END IF;
-			END IF;
-		END PROCESS;
-		fsmc_nrd_edge <= '1' WHEN fsmc_nrd = '1' AND fsmc_was_read_r = '1' ELSE '0';
-
 		-- Select between the ram data, and their mem size.
-		fsmc_output_data <= w_data WHEN fsmc_want_count = '0' ELSE d_count;
+		fsmc_output_data <= r_data WHEN fsmc_want_count = '0' ELSE d_count;
 
 		-- Write data on fsmc when fsmc_nrd = '0' and fsmc_ce = '1'. If not, put high impedance to read.
-		-- fsmc_db <= fsmc_output_data WHEN fsmc_was_read_r = '1' ELSE (OTHERS => 'Z');
 		fsmc_db <= fsmc_output_data WHEN (fsmc_nrd = '0' AND fsmc_ce = '1') ELSE (OTHERS => 'Z');
 END Behavior;
